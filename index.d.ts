e5642138056b1d9d524a68762f8dc1b84bbb1425
VitorMac10 <vitormac3@hotmail.com>
2020-04-09 21:47:14 -0300

    Definitions

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,9 @@
-export interface SoundCloud {
-
-    (client_id: string): SoundCloud;
+declare class SoundCloud {
 
     client_id: string;
 
+    constructor(client_id: string);
+
 }
+
+export = SoundCloud;
