f126d590a3ca59cbe6b7a1a86286149596e7a7a3
VitorMac10 <vitormac3@hotmail.com>
2020-04-09 22:38:14 -0300

    Module documentation

--- index.d.ts
+++ index.d.ts
@@ -2,12 +2,29 @@ declare class SoundCloud {
 
     client_id: string;
 
+    /**
+     * Creates a new instance of the SoundCloud API wrapper
+     * @param client_id Your SoundCloud API key
+     */
     constructor(client_id: string);
 
-    async search(query: string, limit?: number): Array;
+    /**
+     * Searches for tracks
+     * @param query The query to search for
+     * @param limit The limit value for the returned results
+     */
+    async search(query: string, limit?: number): Array<object>;
 
+    /**
+     * Get any track metadada by given id
+     * @param track_id The track's unique id
+     */
     async getTrack(track_id: string): object;
 
+    /**
+     * Returns an array of URLs to stream from
+     * @param track The track metadata object
+     */
     async getTrackTranscodings(track: object): Array<string>;
 
 }
